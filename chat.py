23df304ebfda5447a032ceb8e843908c0b8aef71
Ayush Gupta <127961367+AyusGup@users.noreply.github.com>
2024-09-04 23:08:40 +0530

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,18 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling 
+from torch.utils.data import Dataset, DataLoader 
+import torch
+
+model = AutoModelForCausalLM.from_pretrained("./fine-tuned-dialoGPT") 
+tokenizer = AutoTokenizer.from_pretrained("./fine-tuned-dialoGPT")
+
+def get_response(input_text):
+    # Tokenize the input text and append the end-of-sequence token
+    input_ids = tokenizer.encode(input_text + tokenizer.eos_token, return_tensors="pt")
+
+    # Generate the output using the model
+    output_ids = model.generate(input_ids, max_length=100)
+
+    # Decode the output to get the response text
+    response = tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    
+    return response
