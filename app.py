c2c702e71082ff45b0c51919fa17d360e2960428
Hardik2972 <garghardik2972@gmail.com>
2024-09-05 13:33:38 +0530

    done

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
-from flask import Flask,jsonify,request
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from chat import get_response
-# import os
+import os  # Make sure to import os to access environment variables
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-
 @app.get('/')
 def get_data():
     data = {
-        "message":"Hello this is api end point"
+        "message": "Hello, this is the API endpoint"
     }
     return jsonify(data)
-
 
 @app.post('/predict')
 def predict():
     text = request.get_json().get("message")
     if text == "hi":
-        return jsonify({"answer":"hello, how are you?"})
+        return jsonify({"answer": "hello, how are you?"})
     print(text)
     response = get_response(text)
     print(response)
     message = {"answer": response}
     return jsonify(message)
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5002)
+    # Get host, port, and debug settings from environment variables or set defaults
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = int(os.getenv('FLASK_PORT', 5000))
+    debug = os.getenv('FLASK_DEBUG', 'True').lower() == 'true'
+
+    app.run(host=host, debug=debug, port=port)
